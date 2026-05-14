83180b59470c8c0b5047668c5bf08bb9c3cb856f
Naty Macias <st4441@utr.edu.mx>
2026-05-13 18:58:31 -0600

    Clase 1

---
+++ Modulo-1/Clase-01/tipos_de_datos.js
@@ -0,0 +1,9 @@
+console.log(typeof 42); //number
+console.log(typeof 'Veinticinco0'); //string
+console.log(typeof -666); //number
+console.log(typeof true); //boolean
+console.log(typeof 0); // number
+console.log(typeof ""); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof false); // boolean 
