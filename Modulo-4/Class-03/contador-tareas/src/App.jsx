84a93e53bad837fb2fe95f64f1f688c4a0bc67ae
Naty Macias <st4441@utr.edu.mx>
2026-09-06 22:58:32 -0600

    Desafíos adicionales

--- Modulo-4/Class-03/contador-tareas/src/App.jsx
+++ Modulo-4/Class-03/contador-tareas/src/App.jsx
@@ -1,9 +1,40 @@
 import { useState, useEffect, useMemo } from 'react';
+import './App.css';
 
 function App() {
-  const [tareas, setTareas] = useState([]); // cambia cuando se agrega una tarea
+  const [tareas, setTareas] = useState(() => {
+    const guardadas = localStorage.getItem('tareas'); // tareas es la clave para traer las tareas
+    if (guardadas) {
+      return JSON.parse(guardadas); // converitr el texto de nuevo a un array
+    }
+    return [];
+  }); // cambia cuando se agrega una tarea
+
   const [nuevaTarea, setNuevaTarea] = useState(''); //  cambia cuando se escribe en el input
+
   const [duracion, setDuracion] = useState('');
+
+  const [filtro, setFiltro] = useState('todas');
+
+  // función para filtrar tareas segun su duración
+  const tareasFiltradas = useMemo(() => {
+      switch (filtro) {
+      case 'cortas':
+        return tareas.filter((tarea) => tarea.duracion <= 30);
+
+      case 'largas':
+        return tareas.filter((tarea) => tarea.duracion > 30);
+
+      default:
+        return tareas; // para regresar todas las tareas
+    }
+  }, [tareas, filtro]);
+
+  // efecto para  guardar en localStorage
+  useEffect(() => {
+    localStorage.setItem('tareas', JSON.stringify(tareas));
+  }, [tareas]); // se usa stringify para convertir el valor a texto proque localSorage solo gurarda strings
+  
 
   // Cálculo de tiempo total optimizado con useMemo
   // la función se movio antes del useEfect para no estar en la temporal dead zone
@@ -31,32 +62,41 @@ function App() {
   };
 
   return (
-    <div>
+    <div className='container'>
       <h1>Contador de Tareas</h1>
-      <div>
-        <input 
-          type="text" 
-          value={nuevaTarea} 
-          onChange={(e) => setNuevaTarea(e.target.value)} 
-          placeholder="Nombre de la tarea" 
+      <div className='formulario'>
+        <input
+          type="text"
+          value={nuevaTarea}
+          onChange={(e) => setNuevaTarea(e.target.value)}
+          placeholder="Nombre de la tarea"
         />
-        <input 
-          type="number" 
-          value={duracion} 
-          onChange={(e) => setDuracion(e.target.value)} 
-          placeholder="Duración en minutos" 
+        <input
+          type="number"
+          value={duracion}
+          onChange={(e) => setDuracion(e.target.value)}
+          placeholder="Duración en minutos"
         />
-        <button onClick={agregarTarea}>Agregar tarea</button>
+        <button className='button' onClick={agregarTarea}>Agregar tarea</button>
       </div>
 
       <h2>Tareas</h2>
-      <ul>
-        {tareas.map((tarea, index) => (
-          <li key={index}>{tarea.nombre}: {tarea.duracion} minutos</li>
+      <ul className='lista-tareas'>
+        {tareasFiltradas.map((tarea, index) => (
+          <li className='tarea' key={index}>{tarea.nombre}: {tarea.duracion} minutos</li>
         ))}
       </ul>
 
-      <h3>Total de tiempo: {calcularTiempoTotal} minutos</h3>
+      <h3 className='title'>Total de tiempo: {calcularTiempoTotal} minutos</h3>
+
+      <select
+        className='filtro'
+        value={filtro}
+        onChange={(e) => setFiltro(e.target.value)}>
+        <option value="todas">todas</option>
+        <option value="cortas">cortas</option>
+        <option value="largas">largas</option>
+      </select>
     </div>
   );
 }
